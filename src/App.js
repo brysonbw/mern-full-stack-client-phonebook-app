af49dd2d92136aff8aff2963c36a0c486e9a4d29
“bryson” <brysonward77@gmail.com>
2021-10-27 19:26:41 -0400

    Deleted RR Dom > using window.reload -refresh edit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import './index.css';
 import axios from 'axios';
 import { useState } from 'react';
-import { BrowserRouter as Router, Route, Switch, Link, Redirect } from 'react-router-dom';
 import ContactList from './components/ContactList';
 import AddContact from './components/AddContact';
-import EditContact from './components/EditContact'
+
 
 function App() {
   const [ contacts, setContacts ] = useState([]) 
@@ -24,20 +23,11 @@ setContacts(contacts.filter(contact => contact.id !== id));
 
   return (
     <>
-    <Router path='/'>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <a class="navbar-brand m-2" href="/">Phonebook App</a>
   </nav>
   <AddContact contacts={contacts} setContacts={setContacts} /> 
   <ContactList contacts={contacts} setContacts={setContacts} deleteContact={deleteContact} />
-  <Switch>
-
-     <Route path="/editcontact/:id">
-      <EditContact />
-      </Route>
-
-  </Switch>
-    </Router>
     </>
   );
 }
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,6 +1,5 @@
 import { useEffect } from 'react'
 import axios from 'axios'
-import {Link } from "react-router-dom";
 import EditContact from './EditContact';
 
 
